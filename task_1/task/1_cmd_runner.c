61ce42218cd11f5db90c7f5846ef0f5cf5713b29
Daria Schek <sdasha81@yandex.ru>
2017-09-22 11:05:21 +0300

    tasks 0, 1

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,3 +9,29 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h> 
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char * argv[], char * envp[])
+{
+	const pid_t pid = fork();
+	if (pid < 0) {
+		printf("fork() error\n");
+		return -1;
+	}
+	if (pid) {
+		int status;
+		waitpid(pid, &status, 0);
+		printf("Ret code: %d\n", WEXITSTATUS(status));
+	} else {
+		execvp(argv[1], &argv[1]);  //выйдет через return
+		printf("Error on start\n"); //если не запустилось, то 42
+		exit(42);
+	}
+	return 0;
+}
