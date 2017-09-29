8d78c98d631c60b374cf56779b67ae6e23cd130a
Daria Schek <sdasha81@yandex.ru>
2017-09-30 00:12:51 +0300

    redone

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -16,22 +16,58 @@
 #include <unistd.h> 
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+
 
 int main(int argc, char * argv[], char * envp[])
 {
-	const pid_t pid = fork();
-	if (pid < 0) {
-		printf("fork() error\n");
-		return -1;
-	}
-	if (pid) {
-		int status;
-		waitpid(pid, &status, 0);
-		printf("Ret code: %d\n", WEXITSTATUS(status));
-	} else {
-		execvp(argv[1], &argv[1]);  //выйдет через return
-		printf("Error on start\n"); //если не запустилось, то 42
-		exit(42);
+	printf("Hello. Use 'q' for exit.\n");
+	int q = 0;
+	while (q == 0) {
+		pid_t pid = fork();
+		if (pid < 0) {
+			printf("fork() error\n");
+			return -1;
+		}
+		if (pid) {
+			int status;
+			waitpid(pid, &status, 0);
+			int a = WEXITSTATUS(status);
+			if (a != 255) {
+				printf("Ret code: %d\n", a);
+			//	break;
+			}
+			else {
+				printf("See you.\n");
+				q = 1;
+			//	break;
+			}
+			
+		} else {
+			char * str = (char *) malloc (255 * sizeof(char));
+			char * * args = (char * *) malloc (4096 * sizeof(char *));
+			int i;
+			char s[255];
+			
+			gets(str, 255, stdin);
+			
+			if (strcmp(str, "q") == 0)
+				exit(255);
+			else {
+			
+				for (i = 0; strlen(str) > 0; i++) {
+					sscanf(str, "%s", s);
+					args[i] = (char *) malloc (255 * sizeof(char));
+					strcpy(args[i], s);
+					str += strlen(s)+1;
+				}
+						
+				execvp(args[0], &args[0]);  
+				printf("Error on start\n");
+				exit(42);
+				
+			}
+		}
 	}
 	return 0;
 }
