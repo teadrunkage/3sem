f96f35dd3a68d1dcdea1b47f52118a5c90477413
Daria Schek <sdasha81@yandex.ru>
2017-11-19 00:01:05 +0300

    task3 done

---
+++ task_3/task3A.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+
+#define N 10000
+#define n 2
+
+//int result[n] = {};
+
+
+double f(double x) {
+	return x*x;
+}
+
+int point() {
+	double x = (double) rand()/RAND_MAX; //0 < x < 1
+	double y = (double) rand()/RAND_MAX; //0 < y < 1
+	if (y - f(x) < 0)
+		return 1;
+	else
+		return 0;
+}
+
+
+void * count (void * number) {
+	int * j = number;
+	int jj = *j;
+	
+	int shmid;
+	char pathname[] = "easy_to_type";
+	key_t key = ftok(pathname, 0);
+	shmid = shmget(key, n*sizeof(int), 0);
+	
+	int * result = (int *)shmat(shmid, NULL, 0);
+	
+	int i;
+	int k = round(N/n);
+	for (i = 0; i < k; i++) {
+		result[jj] += point();
+	}
+	shmdt(result);
+	return 0;
+}
+
+
+int main(void) {
+	
+	int shmid;
+	char pathname[] = "easy_to_type";
+	key_t key = ftok(pathname, 0);
+	printf("%d\n", key);
+	shmid = shmget(key, n*sizeof(int), 0666|IPC_CREAT|IPC_EXCL);
+	printf("%d\n", shmid);
+	
+	int * result = (int *)shmat(shmid, NULL, 0);
+	int i;
+	for (i = 0; i < n; i++) {
+		result[i] = 0;
+	}
+	
+	
+	
+	srand(time(NULL));
+	
+	pthread_t* threads;
+	int threads_number = n;
+	
+	threads = (pthread_t*) malloc(threads_number * sizeof(pthread_t));
+	int * numbers = (int *) malloc(threads_number * sizeof(pthread_t));
+	for (i = 0; i < threads_number; i++)
+	{
+		numbers[i] = i;
+		pthread_create( &(threads[i]), NULL, count, &numbers[i]);
+	}
+	for (i = 0; i < threads_number ; i++)
+	{
+		void * res = NULL;
+		pthread_join(threads[i], &res);
+	}
+	free(threads);
+	
+	
+	return 0;
+}
---
+++ task_3/task3B.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <time.h>
+#include <math.h>
+
+#define N 10000
+#define n 2
+
+
+int main(void) {
+	
+	int shmid;
+	char pathname[] = "easy_to_type";
+	key_t key = ftok(pathname, 0);
+	printf("%d\n", key);
+	shmid = shmget(key, n*sizeof(int), 0);
+	printf("%d\n", shmid);
+	
+	int * result = (int *)shmat(shmid, NULL, 0);
+	
+	int i;
+	double b = 0;
+	for (i = 0; i < n; i++) {
+		b += result[i];
+	}
+	printf("%f\n", (double) b/N);
+	
+	shmdt(result);
+	return 0;
+}
